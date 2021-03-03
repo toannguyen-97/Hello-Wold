cfff6588adbfbb4f39c3e8527b6bb31685fddcb9
Toan Nguyen <toannguyen07997@gmail.com>
2021-03-03 16:26:38 +0700

    hello

---
+++ Shared/Hello1App.swift
@@ -0,0 +1,17 @@
+//
+//  Hello1App.swift
+//  Shared
+//
+//  Created by Van Nguyen on 3/3/21.
+//
+
+import SwiftUI
+
+@main
+struct Hello1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
