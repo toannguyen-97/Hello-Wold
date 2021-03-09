e65457cc7b69a517d9617b2a86b9f8bbb642fba4
Toan Nguyen <toannguyen07997@gmail.com>
2021-03-09 16:39:39 +0700

    no message

---
+++ sample/Shared/sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleApp.swift
+//  Shared
+//
+//  Created by Van Nguyen on 3/4/21.
+//
+
+import SwiftUI
+
+@main
+struct sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
